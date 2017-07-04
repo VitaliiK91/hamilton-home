83e8c9526de6484e67e505d53bfc8807aea5a66b
vitalii kalinin <vitaliik@civicresource.com>
2017-07-03 23:05:06 -0700

    tweak home style

--- src/Home.js
+++ src/Home.js
@@ -11,24 +11,24 @@ import './Home.css';
 const Home = () => (
 		<Grid fluid>
 			<Row>
-				<Col xs={12} md={6}>
+				<Col sm={12} md={6}>
 					<Panel header="About Us" className="drop-shadow">
 						<div className="text-container">
 							<p>Since 1999, Hamilton Home has been providing affordable housing for International students, interns, tourists, just about anyone in need of a temporary home in San Diego. Hamilton Home is a pleasant alternative to hostels, hotels, and home stay programs. If you are relocating to San Diego, stay with us and explore this wonderful city, before committing to a long term lease</p>
 						</div>
 					</Panel>
 				</Col>
-				<Col xs={12} md={6}>
+				<Col sm={12} md={6}>
 					<div className="drop-shadow">
 						<img className="main-img" src={main} alt="" />
 					</div>
 				</Col>
-				<Col xs={12} md={6}>
+				<Col sm={12} md={6}>
 					<div className="drop-shadow">
 						<img className="main-img" src={front_house} alt="" />
 					</div>
 				</Col>
-				<Col xs={12} md={6}>
+				<Col sm={12} md={6}>
 					<div className="drop-shadow">
 						<img className="main-img" src={street} alt="" />
 					</div>
